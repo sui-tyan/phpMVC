3152a734a97e0a23abed7fbee30cb44c8ae2d0d7
unknown <CIT183G-21@M303.uc-bcf.edu.ph>
2023-02-15 16:44:20 +0800

    database migration to sqlite, rendering data to blade

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,3 +1,20 @@
+<table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Contact Number</th>
+        <th>Address</th>
+    </tr>
 @foreach ($customers as $customer)
-{{$customer->firstName}}
+<tr>
+    <th>{{$customer->firstName}}</th>
+    <th>{{$customer->lastName}}</th>
+    <th>{{$customer->email}}</th>
+    <th>{{$customer->contactNumber}}</th>
+    <th>{{$customer->address}}</th>
+  </tr>
 @endforeach
+  
+</table>
+
